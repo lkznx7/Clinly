13d752b059fbf74a2ac7580d67617f31ffbb3f3b
lkznx7 <lucas.kzmx@gmail.com>
2026-07-09 19:02:31 -0300

    chore : Upload setup project

---
+++ src/test/java/com/clinly/clinly/ClinlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinly.clinly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
